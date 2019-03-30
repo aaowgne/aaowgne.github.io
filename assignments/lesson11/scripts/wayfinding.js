3c02cae5ee3e175a48f28d581385ebf69425fc94
AAOWGNE <aaowgne@gmail.com>
2019-03-29 23:43:25 -0600

    final doc

--- assignments/lesson11/scripts/wayfinding.js
+++ assignments/lesson11/scripts/wayfinding.js
@@ -3,7 +3,7 @@ var urlArray = urlString.split('/');
 var pageHREF = urlArray[urlArray.length-1];
 
 if (pageHREF !=="") {
-    var menu = document.querySelectorAll('ul.primaryNav li a');
+    var menu = document.querySelectorAll('ul#primaryNav li a ');
     var i;
     for(i=0; i<menu.length; i++) {
         var currentURL=(menu[i].getAttribute("href"));
