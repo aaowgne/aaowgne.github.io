a9928c92cb00e38ba72c3d5ecc16cb947fef7f0d
AAOWGNE <aaowgne@gmail.com>
2019-04-14 17:01:19 -0600

    JSON

--- final-project/scripts/logan.js
+++ final-project/scripts/logan.js
@@ -56,30 +56,30 @@ else dir = "NW"
 document.getElementById("windDirection").textContent=dir;
 }
 var article = document.querySelector('article');
-var requestURL = 'https://aaowgne.github.io/final-project/towndata.json';
+var requestURL = 'https://aaowgne.github.io/final-project/templedata.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function () {
-    var townData = request.response;
-    showData(townData);
+    var templeData = request.response;
+    showData(templeData);
 }
 function showData(jsonObj) {
-    var data = jsonObj['towns'];
+    var data = jsonObj['temple'];
     for (var i = 0; i < data.length; i++) {
         var name = data[i].name;
-        if ((name.includes("Preston")) == false) {
+        if ((name.includes("Logan")) == false) {
             continue;
         }
        
         var myDiv = document.createElement('div');
         var myList = document.createElement('ul');        
-        var townEvents = data[i].events;
+        var templeEvents = data[i].events;
        
-        for (var j = 0; j < townEvents.length; j++) {
+        for (var j = 0; j < templeEvents.length; j++) {
             var listItem = document.createElement('li');
-            listItem.textContent = townEvents[j];
+            listItem.textContent = templeEvents[j];
             myList.appendChild(listItem);
         }
         
