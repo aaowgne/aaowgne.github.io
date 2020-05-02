31d0563165719e368317fe96d5284f31bfe5f7c8
AAOWGNE <aaowgne@gmail.com>
2020-05-02 17:13:48 -0600

    330

---
+++ wdd330/scripts/index.js
@@ -0,0 +1,17 @@
+function getInput() {
+    let input = Number(document.getElementById("input").value);
+    document.getElementById("display-something").innerText = sumAllNumbers(input);
+  }
+  function sumAllNumbers(num) {
+    let total = 0;
+    for (var i = num; i >= 0; --i) {
+      console.log(i);
+      total += i;
+    }
+    return total;
+  }
+  function displaySum() {
+    let input = Number(document.getElementById("input").value);
+    let input2 = Number(document.getElementById("input2").value);
+    document.getElementById("display-something").innerText = input + input2;
+  }
